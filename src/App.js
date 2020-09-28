eecde0e9bab083bb943eafca49edc3a01e6c74c2
oldiepuppie <lebikebihebi@gmail.com>
2020-09-29 03:28:04 +0900

    feat: test heading

--- src/App.js
+++ src/App.js
@@ -1,65 +1,20 @@
 import React from 'react';
-import { Grommet, Header, Anchor } from 'grommet';
+import { Grommet, Header, Heading, Anchor } from 'grommet';
 import { grommet } from 'grommet/themes';
 
-// const customBreakpoints = {
-//   global: {
-//     breakpoints: {
-//       xsmall: {
-//         value: 375,
-//       },
-//       small: {
-//         value: 568,
-//         edgeSize: {
-//           none: '0px',
-//           small: '6px',
-//           medium: '12px',
-//           large: '24px',
-//         },
-//       },
-//       medium: {
-//         value: 768,
-//         edgeSize: {
-//           none: '0px',
-//           small: '12px',
-//           medium: '24px',
-//           large: '48px',
-//         },
-//       },
-//       large: {
-//         value: 1024,
-//         edgeSize: {
-//           none: '0px',
-//           small: '12px',
-//           medium: '24px',
-//           large: '48px',
-//         },
-//       },
-//       xlarge: {
-//         value: 1366,
-//         edgeSize: {
-//           none: '0px',
-//           small: '12px',
-//           medium: '24px',
-//           large: '48px',
-//         },
-//       },
-//     },
-//   },
-// };
-
 function App() {
-  return ( 
-    <div>
-      <Grommet theme={grommet}>
-        <Header background="dark-1" pad="xsmall" height="xsmall">
-          <Anchor
-            href="#"
-            label="sample"
-          />
-        </Header>
-      </Grommet>
-    </div>
+  return (
+    <Grommet theme={grommet}>
+      <Header background="dark-1" pad="xsmall" height="xsmall">
+        <Heading level='1' size='small'>
+          Show yourself
+        </Heading>
+        <Anchor
+          href="#"
+          label="sample"
+        />
+      </Header>
+    </Grommet>
   )
 }
 
