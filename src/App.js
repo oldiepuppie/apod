57c4aaeef9fe3e87db8f0bfcb1cf4ad4b231c0dd
oldiepuppie <lebikebihebi@gmail.com>
2020-09-29 02:57:26 +0900

    feat: test2-responsive page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Grommet, Box, ResponsiveContext, Heading } from 'grommet';
 const customBreakpoints = {
   global: {
     breakpoints: {
+      xsmall: {
+        value: 375,
+      },
       small: {
         value: 568,
         edgeSize: {
@@ -13,8 +16,26 @@ const customBreakpoints = {
           large: '24px',
         },
       },
+      medium: {
+        value: 768,
+        edgeSize: {
+          none: '0px',
+          small: '12px',
+          medium: '24px',
+          large: '48px',
+        },
+      },
       large: {
         value: 1024,
+        edgeSize: {
+          none: '0px',
+          small: '12px',
+          medium: '24px',
+          large: '48px',
+        },
+      },
+      xlarge: {
+        value: 1366,
         edgeSize: {
           none: '0px',
           small: '12px',
@@ -28,11 +49,11 @@ const customBreakpoints = {
 
 function App() {
   return ( 
-    <Grommet theme={customBreakpoints} full>
+  <Grommet theme={customBreakpoints} full>
     <ResponsiveContext.Consumer>
       {size => (
         <Box fill background="dark-1">
-          {/* <Heading>{`Hi, I'm ${size}, resize me!`}</Heading> */}
+          <Heading>{`Hi, I'm ${size}, resize me!`}</Heading>
           My name is Doldol
         </Box>
       )}
