652cb17a8d7455aaebae21c31ff8dd31653af5e1
oldiepuppie <lebikebihebi@gmail.com>
2020-09-29 03:18:10 +0900

    feat: header test

--- src/App.js
+++ src/App.js
@@ -1,64 +1,64 @@
 import React from 'react';
-import { Grommet, Box, ResponsiveContext, Heading } from 'grommet';
+import { Grommet, Header } from 'grommet';
 
-const customBreakpoints = {
-  global: {
-    breakpoints: {
-      xsmall: {
-        value: 375,
-      },
-      small: {
-        value: 568,
-        edgeSize: {
-          none: '0px',
-          small: '6px',
-          medium: '12px',
-          large: '24px',
-        },
-      },
-      medium: {
-        value: 768,
-        edgeSize: {
-          none: '0px',
-          small: '12px',
-          medium: '24px',
-          large: '48px',
-        },
-      },
-      large: {
-        value: 1024,
-        edgeSize: {
-          none: '0px',
-          small: '12px',
-          medium: '24px',
-          large: '48px',
-        },
-      },
-      xlarge: {
-        value: 1366,
-        edgeSize: {
-          none: '0px',
-          small: '12px',
-          medium: '24px',
-          large: '48px',
-        },
-      },
-    },
-  },
-};
+// const customBreakpoints = {
+//   global: {
+//     breakpoints: {
+//       xsmall: {
+//         value: 375,
+//       },
+//       small: {
+//         value: 568,
+//         edgeSize: {
+//           none: '0px',
+//           small: '6px',
+//           medium: '12px',
+//           large: '24px',
+//         },
+//       },
+//       medium: {
+//         value: 768,
+//         edgeSize: {
+//           none: '0px',
+//           small: '12px',
+//           medium: '24px',
+//           large: '48px',
+//         },
+//       },
+//       large: {
+//         value: 1024,
+//         edgeSize: {
+//           none: '0px',
+//           small: '12px',
+//           medium: '24px',
+//           large: '48px',
+//         },
+//       },
+//       xlarge: {
+//         value: 1366,
+//         edgeSize: {
+//           none: '0px',
+//           small: '12px',
+//           medium: '24px',
+//           large: '48px',
+//         },
+//       },
+//     },
+//   },
+// };
 
 function App() {
   return ( 
-  <Grommet theme={customBreakpoints} full>
-    <ResponsiveContext.Consumer>
-      {size => (
-        <Box fill background="dark-1">
-          <Heading>{`Hi, I'm ${size}, resize me!`}</Heading>
-          My name is Doldol
-        </Box>
-      )}
-    </ResponsiveContext.Consumer>
-  </Grommet>
+    <div>
+      <Grommet theme={grommet}>
+        <Header background="dark-1" pad="large" height="small">
+          <Anchor
+            href="https://tools.grommet.io/"
+            label="sample"
+          />
+        </Header>
+      </Grommet>
+    </div>
   )
 }
 
