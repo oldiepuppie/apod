493f11088d061668cbc643b7040578e56898fa5b
oldiepuppie <lebikebihebi@gmail.com>
2020-10-06 10:00:15 +0900

    meeting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { Anchor, Box, DateInput, Grommet, Header, Heading, Main } from 'grommet';
+import { Anchor, Box, DateInput, Grommet, Header, Heading, Main, Image } from 'grommet';
 import { grommet } from 'grommet/themes';
+import useGetApod from './hooks/useGetApod';
 import React from 'react';
 
 function App() {
+  console.log(useGetApod(2019 - 12 - 31));
   const [value, setValue] = React.useState('');
   const onChange = (event) => {
     const nextValue = event.value;
@@ -17,7 +19,6 @@ function App() {
           <Anchor color='accent-3' href='#' label='Astronomy Picture of Day' />
         </Heading>
       </Header>
-
       <Box align='center' pad='large' background='dark-2'>
         <Box width='medium' background='light-1'>
           <DateInput format='mm/dd/yyyy' value={value} onChange={onChange} />
@@ -25,7 +26,9 @@ function App() {
       </Box>
 
       <Main full background='dark-2' pad='small'>
-        Main
+        <Box height='small' width='small'>
+          <Image fit='cover' src='//v2.grommet.io/assets/Wilderpeople_Ricky.jpg' />
+        </Box>
       </Main>
     </Grommet>
   );
---
+++ src/components/date-view/APOD.js
@@ -0,0 +1,4 @@
+// import React, { useState, useEffect } from 'react';
+// import { useGetApod } from './hooks/useGetApod';
+
+// export default APOD;
