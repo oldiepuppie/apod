a872639ddf75ec0a603e712db70e431bb96b218e
oldiepuppie <lebikebihebi@gmail.com>
2020-10-07 11:01:42 +0900

    changed the name to DateViewContainer

--- src/containers/date-view/DateViewContainer.js
+++ src/containers/date-view/DateViewContainer.js
@@ -3,7 +3,12 @@ import APOD from './components/date-view/APOD';
 import DatePicker from './components/date-view/DatePicker';
 
 const DateView = () => {
-  DatePicker();
+  const [date, setDate] = useState(new Date().toISOString());
+  const onDateInputChange = (event) => {
+    const nextDate = event.value;
+    setDate(nextDate);
+  };
+
   const dateForEntry = date.substr(0, 10);
   APOD(dateForEntry);
 };
