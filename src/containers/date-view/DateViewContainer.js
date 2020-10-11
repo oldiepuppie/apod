9492191d095dfda495bdeef56b85aeb14ee61c70
oldiepuppie <lebikebihebi@gmail.com>
2020-10-12 01:52:54 +0900

    removed console.log on the Container

--- src/containers/date-view/DateViewContainer.js
+++ src/containers/date-view/DateViewContainer.js
@@ -13,7 +13,6 @@ const DateViewContainer = () => {
   };
 
   const dateInstance = new Date(isoDate);
-  console.log(dateInstance);
   // 날짜를 데이트 형식으로 변환
 
   const yy = dateInstance.getFullYear();
@@ -23,18 +22,13 @@ const DateViewContainer = () => {
   const dd = dateInstance.getDate() >= 10
             ? dateInstance.getDate()
             : `${ '0' + dateInstance.getDate() }`;
-  console.log(yy);
-  console.log(mm);
-  console.log(dd);
   // 날짜를 YY-MM-DD로 표시하기 위한 변수 할당
   
   const dateForApod = `${yy}-${mm}-${dd}`;
   console.log(dateForApod);
-  // 위의 변수를 useGetApod 훅에 사용할 수 있도록 변환
 
   // useGetApod start
   const apodData = useGetApod(dateForApod);
-  console.log(apodData);
   // useGetApod end
 
   
