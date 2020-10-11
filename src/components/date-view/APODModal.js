7b6e7cd55f8f3f740b383b5e8456e9a393e79cd0
oldiepuppie <lebikebihebi@gmail.com>
2020-10-12 02:09:11 +0900

    designed modal box

--- src/components/date-view/APODModal.js
+++ src/components/date-view/APODModal.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Box } from 'grommet';
+
+import { Box, Button, Layer } from 'grommet';
+import React, { useState } from 'react';
+
 
 const APODModal = ({ title, date, explanation, closeModal }) => {
+  const [show, setShow] = useState();
 
   return (
-    <div>
-      <Box>{title}</Box>
-      <Box>{date}</Box>
-      <Box>{explanation}</Box>
-      <Box>{closeModal}</Box>
+      <div>
+        <Box>
+          <Button label="show" onClick={() => setShow(true)} />
+          {show && (
+            <Layer
+              onEsc={() => setShow(false)}
+              onClickOutside={() => setShow(false)}
+            >    
+              <Box align='center' pad='large' background='dark-1'>
+                <Box>{title}</Box>
+                <Box>{date}</Box>
+                <Box>{explanation}</Box>
+                <Box>{closeModal}</Box>
+              </Box>
+              <Button label="close" onClick={() => setShow(false)} />
+            </Layer>
+          )};
+        </Box>
     </div>
   );
 };
