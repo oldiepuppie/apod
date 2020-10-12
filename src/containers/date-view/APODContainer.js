eb3b595c2f2abf016015eb674d2f0b6569aff493
oldiepuppie <lebikebihebi@gmail.com>
2020-10-13 00:26:29 +0900

    added media_type prop

--- src/containers/date-view/APODContainer.js
+++ src/containers/date-view/APODContainer.js
@@ -1,7 +1,7 @@
 import { Box, Image, Layer, Text } from 'grommet';
 import React, { useState } from 'react';
 
-const APODContainer = ({ title, date, explanation, url }) => {
+const APODContainer = ({ title, date, explanation, url, media_type }) => {
   const [ isAPODModalShow, setIsAPODModalShow ] = useState();
   setIsAPODModalShow(false);
 
@@ -9,28 +9,29 @@ const APODContainer = ({ title, date, explanation, url }) => {
 
   return (
     <div>
-      mediaType === 'image'
-      ? (
-        <Box align='center' pad='large' background='dark-2'>
-          <Image fit='cover' src={url} onClick={setIsAPODModalShow(true)}></Image>
-          {isAPODModalShow && (
-            <Layer
-              onEsc={() => setIsAPODModalShow(false)}
-              onClickOutside={() => setIsAPODModalShow(false)}
-            >
-              <Box align='center' pad='medium' background='dark-1'>
-                <Box pad='small'>{title}</Box>
-                <Box pad='small'>{date}</Box>
-                <Box pad='small'>{explanation}</Box>
-              </Box>
-                <Text color='status-critical' onClick={() => setIsAPODModalShow(false)}>X</Text>
-            </Layer>
-          )}
-        </Box>
-        )
-      : (
-        <iframe title={title} src={url} frameBorder="0" gesture="media" allow="encrypted-media" allowFullScreen></iframe>
-      )
+      {media_type === 'image'
+        ? (
+          <Box align='center' pad='large' background='dark-2'>
+            <Image fit='cover' src={url} onClick={setIsAPODModalShow(true)}></Image>
+            {isAPODModalShow && (
+              <Layer
+                onEsc={() => setIsAPODModalShow(false)}
+                onClickOutside={() => setIsAPODModalShow(false)}
+              >
+                <Box align='center' pad='medium' background='dark-1'>
+                  <Box pad='small'>{title}</Box>
+                  <Box pad='small'>{date}</Box>
+                  <Box pad='small'>{explanation}</Box>
+                </Box>
+                  <Text color='status-critical' onClick={() => setIsAPODModalShow(false)}>X</Text>
+              </Layer>
+            )}
+          </Box>
+          )
+        : (
+          <iframe title={title} src={url} frameBorder="0" gesture="media" allow="encrypted-media" allowFullScreen></iframe>
+          )
+        }
     </div>
   );
 };
