7ab3006131458bfc85ed594b90223b49b9d3ea6e
oldiepuppie <lebikebihebi@gmail.com>
2020-10-12 22:03:00 +0900

    #26 reflect feedback on DateViewContainer

--- src/containers/date-view/DateViewContainer.js
+++ src/containers/date-view/DateViewContainer.js
@@ -7,31 +7,25 @@ import APODModal from './../../components/date-view/APODModal';
 const DateViewContainer = () => {
   const [isoDate, setIsoDate] = useState(new Date().toISOString());
 
-  // 날짜 입력
   const onDateInputChange = (event) => {
     const nextIsoDate = event.value;
     setIsoDate(nextIsoDate);
   };
 
   const dateInstance = new Date(isoDate);
-  // 날짜를 데이트 형식으로 변환
 
-  const yy = dateInstance.getFullYear();
-  const mm = dateInstance.getMonth() + 1 >= 10 
+
+  const yyyy = dateInstance.getFullYear();
+  const mm = dateInstance.getMonth() + 1 >= 10
             ? dateInstance.getMonth() + 1
-            : `${ '0' + (dateInstance.getMonth() + 1) }`;
+            : `0${dateInstance.getMonth() + 1}`;
   const dd = dateInstance.getDate() >= 10
             ? dateInstance.getDate() -1
-            : `${ '0' + (dateInstance.getDate()-1) }`;
-  // 날짜를 YY-MM-DD로 표시하기 위한 변수 할당
-  // time zone 맞추기 필요 : 일단 day에 -1
+            : `0${dateInstance.getDate()-1}`;
   
-  const dateForApod = `${yy}-${mm}-${dd}`;
-  // YY-MM-DD
+  const dateForApod = `${yyyy}-${mm}-${dd}`;
 
-  // useGetApod start
   const apodData = useGetApod(dateForApod);
-  // useGetApod end
 
   
   return (
@@ -42,7 +36,7 @@ const DateViewContainer = () => {
         </Box>
       </Box>
       <Box align='center' pad='large' background='dark-2'>
-        <APODModal 
+        <APODModal
         date={apodData.data.date}
         explanation={apodData.data.explanation}
         title={apodData.data.title}
