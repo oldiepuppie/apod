92e9d4d6fb92528a895124f5727a88fedabd0c95
Heonyoung Kim <altenull@gmail.com>
2020-10-16 20:33:00 +0900

    refactor: apply prettier, refactor code

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import React from 'react';
 import styled from 'styled-components';
 import DateViewContainer from './containers/date-view/DateViewContainer';
 
-const MyStyledMain = styled(Main)
-  `height: calc( 100% - 96px );
+const MyStyledMain = styled(Main)`
+  height: calc(100% - 96px);
   background-color: #555;
-  `;
+`;
 
 function App() {
   return (
     <Grommet theme={grommet} full>
       <Header background='dark-1' pad='medium' height='xsmall'>
-          <Heading level='1' size='small'>
-            <Anchor color='accent-3' href='#' label='Astronomy Picture of Day' />
-          </Heading>
-        </Header>
+        <Heading level='1' size='small'>
+          <Anchor color='accent-3' href='#' label='Astronomy Picture of Day' />
+        </Heading>
+      </Header>
       <MyStyledMain>
         <DateViewContainer />
       </MyStyledMain>
--- src/components/date-view/APODModal.js
+++ src/components/date-view/APODModal.js
@@ -3,20 +3,23 @@ import React from 'react';
 
 const APODModal = ({ title, date, explanation, onClose }) => {
   return (
-      <div>
-          <Layer
-            onEsc={onClose}
-            onClickOutside={onClose}
-          >
-            <Box align='center' pad='medium' background='dark-1'>
-              <Heading alignSelf='start' margin='xsmall' level='3'>{title}</Heading>
-              <Paragraph alignSelf='start' margin='xsmall'>{date}</Paragraph>
-              <Paragraph fill margin='xsmall' size='small'>{explanation}</Paragraph>
-            </Box>
-            <Box background='dark-1' pad='medium' align='center'>
-              <Button label='close 'onClick={onClose} size='large' weight='bold' color='accent-2'/>
-            </Box>
-          </Layer>
+    <div>
+      <Layer onEsc={onClose} onClickOutside={onClose}>
+        <Box align='center' pad='medium' background='dark-1'>
+          <Heading alignSelf='start' margin='xsmall' level='3'>
+            {title}
+          </Heading>
+          <Paragraph alignSelf='start' margin='xsmall'>
+            {date}
+          </Paragraph>
+          <Paragraph fill margin='xsmall' size='small'>
+            {explanation}
+          </Paragraph>
+        </Box>
+        <Box background='dark-1' pad='medium' align='center'>
+          <Button label='close ' onClick={onClose} size='large' weight='bold' color='accent-2' />
+        </Box>
+      </Layer>
     </div>
   );
 };
