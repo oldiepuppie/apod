f2910c4872b327dcadf8307011133f61b6ad63bf
Heonyoung Kim <altenull@gmail.com>
2020-10-16 20:25:59 +0900

    refactor: validate available date range in onDateInputChange function

--- src/containers/date-view/DateViewContainer.js
+++ src/containers/date-view/DateViewContainer.js
@@ -6,19 +6,14 @@ import APODContainer from './APODContainer';
 
 const DateViewContainer = () => {
   const [isoDate, setIsoDate] = useState(new Date().toISOString());
-  const onDateInputChange = (event) => {
-    const nextIsoDate = event.value;
-    setIsoDate(nextIsoDate);
-  }
 
   const validateAvailableDateRange = (isoDate) => {
     const minDate = new Date(1995, 5, 16);
     const maxDate = new Date();
     const targetDate = new Date(isoDate);
 
-    return ( targetDate.getTime() >= minDate.getTime() && targetDate.getTime() <= maxDate.getTime()
-      ? true : false )
-  }
+    return targetDate.getTime() >= minDate.getTime() && targetDate.getTime() <= maxDate.getTime();
+  };
 
   const getAPODDate = (isoDate) => {
     const dateInstance = new Date(isoDate);
@@ -27,14 +22,19 @@ const DateViewContainer = () => {
     const dd = dateInstance.getDate() >= 10 ? dateInstance.getDate() - 1 : `0${dateInstance.getDate() - 1}`;
 
     return `${yyyy}-${mm}-${dd}`;
-  }
-  
-  useEffect(() => {
-    if ( !validateAvailableDateRange(isoDate) )
-    { alert('Date Range : 1995-06-16 ~ Today') }
-  }, [isoDate])
+  };
 
-  const apodData = useGetApod(getAPODDate(isoDate))  
+  const onDateInputChange = (event) => {
+    const nextIsoDate = event.value;
+
+    if (validateAvailableDateRange(nextIsoDate)) {
+      setIsoDate(nextIsoDate);
+    } else {
+      alert('Date Range : 1995-06-16 ~ Today');
+    }
+  };
+
+  const apodData = useGetApod(getAPODDate(isoDate));
 
   return (
     <div>
@@ -44,18 +44,19 @@ const DateViewContainer = () => {
         </Box>
       </Box>
 
-      { apodData.isGetApodLoading 
-        ? <Box align='center' pad='large' background='dark-2'>
-            <ReactLoading type='spin' color='#fff' />
-          </Box>
-        : <APODContainer
-        title={apodData.data.title}
-        date={apodData.data.date}
-        explanation={apodData.data.explanation}
-        url={apodData.data.url}
-        media_type={apodData.data.media_type} />
-      }
-      
+      {apodData.isGetApodLoading ? (
+        <Box align='center' pad='large' background='dark-2'>
+          <ReactLoading type='spin' color='#fff' />
+        </Box>
+      ) : (
+        <APODContainer
+          title={apodData.data.title}
+          date={apodData.data.date}
+          explanation={apodData.data.explanation}
+          url={apodData.data.url}
+          media_type={apodData.data.media_type}
+        />
+      )}
     </div>
   );
 };
