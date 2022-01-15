923b7db063dc2b3074a0b86667877396fe40aa18
이상아 <lebikebihebi@gmail.com>
2022-01-16 04:17:05 +0900

    Feat: Add custom hook to handle list data with indexedDB

---
+++ src/hooks/useIndexedDB.js
@@ -0,0 +1,21 @@
+// TODO indexedDB 작업중
+
+import { useState, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { bookmarkListState } from '../recoil/atoms';
+import { bookmark } from '../recoil/atoms/index';
+
+const useIndexedDB = (date) => {
+  indexedDB.open('bookmarkList', 1);
+  const list = useRecoilValue(bookmarkListState);
+
+  useEffect(() => {
+    const isInList = (list) => {
+      const validItem = list.filter((item) => item.date === date);
+      if (validItem) [validItem, ...list];
+      else [...list.filter((item) => item.date !== date)];
+    };
+  }, [date, list]);
+};
+
+export default useIndexedDB;
